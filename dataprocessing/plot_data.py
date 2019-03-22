f04e61b53a50dc8f9d4a2cc50ba80ddacc6e2b89
Sven Holtrop <yadeses@gmail.com>
2019-03-22 01:20:23 +0100

    updated mapping

--- dataprocessing/plot_data.py
+++ dataprocessing/plot_data.py
@@ -2,19 +2,27 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
+import os
 
-# Load data
-data = np.load('./npys/N43E005.npy')
+def datalist():
+	return map(lambda x: np.load(os.path.join("./npys", x)), os.listdir("./npys"))
 
-# Show image
-plt.imshow(data)
-plt.show()
+def iterate_data(display_func):
+	for i in datalist():
+		display_func(i)	
 
-# 3D-plot
-X, Y = np.meshgrid(range(data.shape[0]), range(data.shape[1]))
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, data, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+def map2d(data):
+	data[data < 0] = 0
+	plt.imshow(data)
+	plt.show()
 
-plt.show()
+def map3d(data, colour=cm.jet):
+	X, Y = np.meshgrid(range(data.shape[0]), range(data.shape[1]))
+	fig = plt.figure()
+	ax = fig.gca(projection='3d')
+	surf = ax.plot_surface(X, Y, data/22, cmap=colour, linewidth=0, antialiased=False)
+	ax.auto_scale_xyz([0, 3601], [0, 3601], [0, 5000/22])
+	plt.show()
+
+if __name__=='__main__':
+	iterate_data(map2d)
